4d8c70b70e2936f48d4f3585292afd9ea828ecb5
biancab8 <bitting@ualberta.ca>
2022-10-05 20:06:55 -0600

    add initial files

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express, { response } from "express";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+
+app.listen(4000, () => {console.log("server running on port 4000")});
