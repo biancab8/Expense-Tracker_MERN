790d7c6e0fb07b836cb4c7cfe2cbd2936b263bd9
biancab8 <bitting@ualberta.ca>
2022-10-06 17:31:13 -0600

    add db connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 import express, { response } from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
+app.use(cors);
+dotenv.config(); 
+
+//connect to DB
+try{
+    await mongoose.connect("mongodb+srv://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@cluster0.igfw9cr.mongodb.net/?retryWrites=true&w=majority");
+    console.log("successfullly connected to mongoDB");
+} catch (err) {
+    console.log("Could not connect to mongoDB. Error: "+err);
+}
 
 
 app.get("/", (req, res) => {
