92f2733c8555bfa646713c9cabcc2243ed334550
biancab8 <bitting@ualberta.ca>
2022-11-18 18:38:03 -0700

    fix responsive design for table

--- client/src/features/ui/table/TableHeaderCell.js
+++ client/src/features/ui/table/TableHeaderCell.js
@@ -4,14 +4,14 @@ import { colors } from "../../../assets";
 export default function TableHeaderCell(props) {
   return !props.nestedCell ? (
     <TableCell
-      sx={{ color: colors.textSecondary, backgroundColor: colors.tableHeader, fontSize: {xs: "0.7rem", md: "0.875rem"}, whiteSpace: "pre-wrap", wordBreak: "keep-all"}}
+      sx={{ color: colors.textSecondary, backgroundColor: colors.tableHeader, fontSize: {xs: "0.7rem", md: "0.875rem"}, whiteSpace: "pre-wrap", wordBreak: "break-word"}}
       align="center"
     >
       {props.text}
     </TableCell>
   ) : (
     <TableCell
-      sx={{ color: colors.textSecondary, backgroundColor: colors.tableHeader, fontSize: {xs: "0.7rem", md: "0.875rem"}, whiteSpace: "pre-wrap", wordBreak: "keep-all"}}
+      sx={{ color: colors.textSecondary, backgroundColor: colors.tableHeader, fontSize: {xs: "0.7rem", md: "0.875rem"}, whiteSpace: "pre-wrap", wordBreak: "break-word"}}
       align="center"
     >
       {props.text} {props.nestedCell}
